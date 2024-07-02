eaa4d7f475f172bd10a8678bfe6f5cf156b534b9
Marta Stefanska <marta.stefanska@outlook.es>
2024-07-02 19:41:44 +0200

    Pierwsza wersja brak walidacji danych

---
+++ TriangleCheck/TriangleCheck/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Witaj w programie Triangle Check! Sprawdź czy podane kąty utworzą trójkąt");
+Console.WriteLine("Wpisz 0 jesli chcesz zakonczyć program");
+Console.WriteLine();
+
+while (true)
+{
+    Console.WriteLine("Podaj wartość kąta Alfa:");
+    var inputAlfa = int.Parse(Console.ReadLine());
+    if(inputAlfa == 0)
+    { break; }
+
+    Console.WriteLine("Podaj wartość kąta Beta:");
+    var inputBeta = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Podaj wartość kąta Gamma:");
+    var inputGamma = int.Parse(Console.ReadLine());
+
+    var sum = inputAlfa + inputBeta + inputGamma;
+    Console.WriteLine($"Suma Kątów wynosi: {sum}");
+
+    if (sum == 180)
+    {
+        Console.WriteLine("Istnieje trójkąt dla podanych kątów. Spróbuj ponownie");
+    }
+    else if (sum != 180)
+    {
+        Console.WriteLine("Nie istnieje trójkąt dla podanych kątów. Spróbuj ponownie.");
+    }
+}
+    
