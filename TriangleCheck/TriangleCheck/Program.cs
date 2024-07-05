3f2ba9d9c7a3382a547c4f7f40dd93ca68d64009
Marta Stefanska <marta.stefanska@outlook.es>
2024-07-05 20:32:00 +0200

    Ulepszony TriangkeCheck

--- TriangleCheck/TriangleCheck/Program.cs
+++ TriangleCheck/TriangleCheck/Program.cs
@@ -1,21 +1,38 @@
-﻿Console.WriteLine("Witaj w programie Triangle Check! Sprawdź czy podane kąty utworzą trójkąt");
-Console.WriteLine("Wpisz 0 jesli chcesz zakonczyć program");
+﻿using System.Drawing;
+
+Console.WriteLine("Witaj w programie Triangle Check! Sprawdź czy podane kąty utworzą trójkąt");
 Console.WriteLine();
+
+List<string> angles = new List<string>()
+{ "Alfa", "Beta", "Gamma" };
+int sumAngle = 0;
+
+List<int> intAngles = new List<int>();
 
 while (true)
 {
-    Console.WriteLine("Podaj wartość kąta Alfa:");
-    var inputAlfa = int.Parse(Console.ReadLine());
-    if(inputAlfa == 0)
-    { break; }
+    intAngles.Clear();
+    
+    foreach (string angle in angles)
+    {     
+        bool invalidInput = true;
+        while (invalidInput)  
+        {
+            Console.WriteLine($"Podaj wartość kąta {angle}:");
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int intAngle) && intAngle > 0 && intAngle < 180)
+            {
+                intAngles.Add(intAngle);
+                invalidInput = false;
+            }
+            else if (intAngle <= 0 && intAngle >= 180)
+            {
+                Console.WriteLine("Nieprawidłowa wartość kąta. Spróbuj ponownie");
+            }           
+        }
+    }
 
-    Console.WriteLine("Podaj wartość kąta Beta:");
-    var inputBeta = int.Parse(Console.ReadLine());
-
-    Console.WriteLine("Podaj wartość kąta Gamma:");
-    var inputGamma = int.Parse(Console.ReadLine());
-
-    var sum = inputAlfa + inputBeta + inputGamma;
+    var sum = intAngles.Sum();
     Console.WriteLine($"Suma Kątów wynosi: {sum}");
 
     if (sum == 180)
@@ -26,5 +43,12 @@ while (true)
     {
         Console.WriteLine("Nie istnieje trójkąt dla podanych kątów. Spróbuj ponownie.");
     }
+
+    Console.WriteLine("Czy chcesz sprawdzić kolejny trójkąt?");
+    string nextTriangle = Console.ReadLine();
+    if (nextTriangle == "Tak" || nextTriangle == "TAK" || nextTriangle == "tak")
+    { continue; }
+    else
+    { break; }
 }
     
